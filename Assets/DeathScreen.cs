369045e25f085c3d4a6da8b82dcc307358bdfbf0
Tom Alner <tomalner18@gmail.com>
2021-10-07 17:31:28 +0100

    Copy all assets

---
+++ Assets/DeathScreen.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DeathScreen : MonoBehaviour {
+    public Text title;
+    public Text body;
+    public Image Icon;
+
+    // Start is called before the first frame update
+    void Start() {
+        GameManager.manager.ResetEvent += Reset;
+    }
+
+    public void Reset() {
+        gameObject.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+
+    public void UpdateDeathScreenText(BadItem badItem) {
+        Icon.sprite = Resources.Load<Sprite>("BadIcons/" + badItem.type.ToString());
+        body.text = string.Format("{0}: {1}\nPress spacebar to continue...", badItem.Title, badItem.description);
+    }
+}
---
+++ Assets/IntroFF.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IntroFF : MonoBehaviour
+{
+    public float sprintSpeed;
+    public float counter;
+    private bool moved;
+    // Start is called before the first frame update
+    void Start()
+    {
+        moved = false;
+        gameObject.SetActive(true);
+        counter = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (!GameManager.manager.GameOver && moved == false && counter < 3)
+        {
+            counter += Time.deltaTime;
+            transform.localPosition += new Vector3(sprintSpeed * Time.deltaTime, 0, 0);
+        }
+        if(counter > 3)
+        {
+            moved = true;
+            gameObject.SetActive(false);
+        }
+    }
+}
